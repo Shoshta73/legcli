9f34416cb081199400d027fd61d529cc05dcefca
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-01-07 07:00:09 +0100

    handle version and info flags

--- cmd/handleArgs.go
+++ cmd/handleArgs.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/Shoshta73/legcli/cmd/initProc"
 	"github.com/Shoshta73/legcli/setup"
 )
+
+const VERSION = "dev-v0"
 
 func HandleArgs(args *CliArgs, initialized bool, filepaths setup.ConfigPaths) {
 	if args.Init {
@@ -11,6 +15,19 @@ func HandleArgs(args *CliArgs, initialized bool, filepaths setup.ConfigPaths) {
 		if !ok {
 			panic("Failed to initialise app")
 		}
+		return
+	}
+	if args.Version || args.Info {
+		fmt.Printf("legcli version: %s", VERSION)
+		if args.Version {
+			return
+		}
+		fmt.Printf("\n")
+		fmt.Printf("Authored by: Borna Šoštarić (Shoshta73) https://github.com/Shoshta73\n")
+		fmt.Printf("https://github.com/Shoshta73/legcli\n")
+		fmt.Printf("\n")
+		fmt.Printf("Copyright (c) 2024 Borna Šoštarić and Contributors.\n")
+
 		return
 	}
 }
